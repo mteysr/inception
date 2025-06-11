601c6aa217763498f433efc03f865d6bf8e41896
Mete Mehmet Yasar <metyasar@vbox.42istanbul.com.tr>
2025-06-11 19:43:43 +0300

    first commit

--- inception-master/srcs/requirements/nginx/tools/setup.sh
+++ inception-master/srcs/requirements/nginx/tools/setup.sh
@@ -4,7 +4,7 @@
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/nginx/ssl/selfsigned.key \
     -out /etc/nginx/ssl/selfsigned.crt \
-    -subj "/C=TR/ST=Istanbul/L=Istanbul/O=42/OU=Student/CN=asim.42.fr"
+    -subj "/C=TR/ST=Istanbul/L=Istanbul/O=42/OU=Student/CN=metyasar.42.fr"
 
 # nginx başlat
 nginx -g "daemon off;"
